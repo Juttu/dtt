f5964e267a0bc0427ccc21a154b4e4d35db19f86
Juttu <69361530+Juttu@users.noreply.github.com>
2022-03-09 03:45:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import random
+import time
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+name=st.session_state.name
+
+i=0
+if name=="dtt":
+    while True:
+        r1 = random.randint(0, 10)
+        for i in range(1):
+            i=i+1
+        st.write(r1*100)
+        print(r1)
+        time.sleep(5)
+    # print("dtt")
+
+    
